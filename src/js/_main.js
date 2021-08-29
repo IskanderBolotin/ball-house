bdd09ad61f2ebe24f63e44565af49d3f512fa1f3
Alexander Bolotin <sanya.bolotin.96@mail.ru>
2021-08-29 12:45:15 +0300

    Добавлена вся верстка

--- src/js/_main.js
+++ src/js/_main.js
@@ -40,24 +40,41 @@ $(document).ready(function(){
             $("[data-modal]").each(function() {
                 if ($(this).attr("data-modal") == this_id) {
                     $(this).addClass("__active");
+                    $(this).find("[data-modal-inner]").addClass("__active");
                 }
                 else {
                     $(this).removeClass("__active");
+                    $(this).find("[data-modal-inner]").removeClass("__active");
                 }
             });
         }
     });
     $("body").on("click", "[data-modal-cls]", function() {
         $(this).parents("[data-modal]").removeClass("__active");
+        $(this).parents("[data-modal]").find("[data-modal-inner]").removeClass("__active");
     });
     $("body").on("mousedown", "[data-modal]", function(e) {
         if (!e.target.closest("[data-modal-inner]")) {
             $(this).removeClass("__active");
-        }нн
+            $(this).find("[data-modal-inner]").removeClass("__active");
+        };
     });
     $("body").on("click", "[data-scroll-top]", function(e) {
         e.preventDefault();
         $('body,html').animate({scrollTop: 0}, 400);
+    });
+    
+    $("body").on("click", "#open-filter", function(e) {
+        $("#collapsed-filter").addClass("__active");
+    })
+    $("body").on("click", "#open-sort", function(e) {
+        $("#mobile-sort").addClass("__active");
+    })
+    $("body").on("click", "[data-cls-sort]", function(e) {
+        $("#mobile-sort").removeClass("__active");
+    });
+    $("body").on("click", "[data-cls-filter]", function(e) {
+        $("#collapsed-filter").removeClass("__active");
     });
     $("body").on("click", "[data-toggle-btn]", function(e) {
         if (!$(this).hasClass("__active")) {
@@ -88,4 +105,38 @@ $(document).ready(function(){
         minimumResultsForSearch: -1,
         width: "100%",
     });
+    if ($(window).outerWidth() <= 575) {
+        let head_h = $(".mainHeader").outerHeight();
+        let filter_h = $("#mobile-filter").outerHeight();
+        $("#mobile-filter").addClass("__sticky");
+        $("#mobile-filter").css("top", head_h + "px");
+        $("#mobile-filter").parents(".filterComponent__mobile").css("min-height", filter_h + "px");
+        $(window).on("scroll", function(params) {
+            if ($("#mobile-filter").hasClass("__sticky")) {
+                if ($("#mobile-filter").offset().top <= $("#mobile-filter").parents(".filterComponent__mobile").offset().top) {
+                    $("#mobile-filter").removeClass("__sticky");
+                }
+            }
+            else {
+                if (($(".mainHeader").offset().top + head_h) >= $("#mobile-filter").offset().top) {
+                    $("#mobile-filter").addClass("__sticky");
+                }
+            }
+        })
+    }
+    $("[type='tel']").mask("+7(999) 999-9999");
+    $("body").on("change", "[data-switch]", function (params) {
+        let this_id = $(this).attr("data-switch");
+        let area = $(this).attr("data-switch-name");
+        $("[data-switch-el]").each(function(params) {
+            if ($(this).attr("data-switch-name") == area) {
+                if ($(this).attr("data-switch-el") == this_id) {
+                    $(this).addClass("__active");
+                }
+                else {
+                    $(this).removeClass("__active");
+                }
+            }
+        })
+    })
 });
